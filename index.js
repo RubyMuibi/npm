e1c7c425ce880d33c8a065c8168a3e0b29591113
Ruby Muibi <ruby@rubymuibi.com>
2024-04-11 10:45:36 -0500

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+const ruby = {
+  role: "Full-stack software developer",
+
+  code: ["JavaScript", "Python", "HTML", "CSS"],
+
+  technologies: {
+    backEnd: ["Node.js", "Express.js"],
+    frontEnd: ["React.js", "Next.js", "Tailwind CSS"],
+    mobileApp: ["React Native"],
+    databases: ["PostgreSQL", "SQLite", "MongoDB"],
+    tests: ["Jest", "Supertest"],
+    devOps: ["Google Cloud Platform", "AWS EC2", "Docker", "GitHub Actions"],
+    design: ["Figma", "Adobe Creative Cloud"],
+  },
+
+  askMeAbout: [
+    "Web development",
+    "Mobile app development",
+    "Technology",
+    "Algebra",
+  ],
+
+  currentFocus: {
+    learning: ["Three.js"],
+    RubyMB: "Relational Database",
+  },
+
+  socials: {
+    web: "https://rubymuibi.com",
+    linkedin: "https://linkedin.com/in/rubymuibi/",
+    twitter: "https://twitter.com/rubymuibi/",
+  },
+
+  goals: "Open-source contributions",
+};
+
+console.log(ruby);
